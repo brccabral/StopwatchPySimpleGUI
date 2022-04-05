5bab3a91d29f32602a900db6dd9a87deab59bae4
Bruno Cabral <cabral.brc@gmail.com>
2022-04-04 21:53:49 -0700

    initial window setup

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import PySimpleGUI as sg
 
+sg.theme("black")
+layout = [
+    [sg.Push(), sg.Image("cross.png", pad=0)],
+    [sg.VPush()],
+    [sg.Text("time", key="-TEXT-")],
+    [
+        sg.Button("Start", key="-BUTTON1-", expand_x=False),
+        sg.Button("Lap", key="-BUTTON2-", expand_x=False),
+    ],
+    [sg.VPush()],
+]
 
-layout = [[]]
-
-window = sg.Window("Title", layout)
+window = sg.Window(
+    "Stowatch",
+    layout,
+    size=(300, 300),
+    no_titlebar=True,
+    element_justification="center",
+    location=(100, 100),
+)
 
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED:
+    if event in (sg.WIN_CLOSED, "-BUTTON1-"):
         break
 
 window.close()
