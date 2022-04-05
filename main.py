f3d68bae52d70038c1ec0c995f954d9ec6775eef
Bruno Cabral <cabral.brc@gmail.com>
2022-04-04 22:18:27 -0700

    reset creates new window

--- main.py
+++ main.py
@@ -1,39 +1,44 @@
 import PySimpleGUI as sg
 import time
 
-sg.theme("black")
-layout = [
-    [sg.Push(), sg.Image("cross.png", pad=0, enable_events=True, key="-CLOSE-")],
-    [sg.VPush()],
-    [sg.Text("time", key="-TIME-", font="Young 50")],
-    [
-        sg.Button(
-            "Start",
-            key="-STARTSTOP-",
-            expand_x=False,
-            button_color=("#FFFFFF", "#FF0000"),
-            border_width=0,
-        ),
-        sg.Button(
-            "Lap",
-            key="-LAP-",
-            expand_x=False,
-            button_color=("#FFFFFF", "#FF0000"),
-            border_width=0,
-            visible=False,
-        ),
-    ],
-    [sg.VPush()],
-]
 
-window = sg.Window(
-    "Stowatch",
-    layout,
-    size=(300, 300),
-    no_titlebar=True,
-    element_justification="center",
-    location=(100, 100),
-)
+def create_window():
+    sg.theme("black")
+    layout = [
+        [sg.Push(), sg.Image("cross.png", pad=0, enable_events=True, key="-CLOSE-")],
+        [sg.VPush()],
+        [sg.Text("time", key="-TIME-", font="Young 50")],
+        [
+            sg.Button(
+                "Start",
+                key="-STARTSTOP-",
+                expand_x=False,
+                button_color=("#FFFFFF", "#FF0000"),
+                border_width=0,
+            ),
+            sg.Button(
+                "Lap",
+                key="-LAP-",
+                expand_x=False,
+                button_color=("#FFFFFF", "#FF0000"),
+                border_width=0,
+                visible=False,
+            ),
+        ],
+        [sg.VPush()],
+    ]
+
+    return sg.Window(
+        "Stowatch",
+        layout,
+        size=(300, 300),
+        no_titlebar=True,
+        element_justification="center",
+        location=(100, 100),
+    )
+
+
+window = create_window()
 
 start_time = 0
 active = False
@@ -50,11 +55,17 @@ while True:
             window["-STARTSTOP-"].update("Reset")
             window["-LAP-"].update(visible=False)
         else:
-            # from start to active
-            start_time = time.time()
-            active = True
-            window["-STARTSTOP-"].update("Stop")
-            window["-LAP-"].update(visible=True)
+            # from stop to reset
+            if start_time > 0:
+                window.close()
+                window = create_window()
+                start_time = 0
+            else:
+                # from start to active
+                start_time = time.time()
+                active = True
+                window["-STARTSTOP-"].update("Stop")
+                window["-LAP-"].update(visible=True)
 
     if active:
         elapsed_time = round(time.time() - start_time, 1)
